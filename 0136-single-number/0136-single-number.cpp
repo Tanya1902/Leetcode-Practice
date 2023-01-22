c4dc0cf6496676308b405a2348521a01a8b8502c
Tanya1902 <54153857+Tanya1902@users.noreply.github.com>
2023-01-23 03:38:35 +0530

    Time: 11 ms (98.67%), Space: 16.9 MB (87.37%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        if(nums.size()==1)
-            return nums[0];
+        int ans=nums[0];
         
-        map<int,int> m;
-        
-        for(int i=0;i<nums.size();i++){
-           m[nums[i]]++;
+        for(int i=1;i<nums.size();i++){
+            ans=ans^nums[i];   
         }
         
-        for(auto e:m){
-           if(e.second==1)
-               return e.first;
-        }
-        
-        return 0;
+        return ans;
     }
 };
